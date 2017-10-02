f82402eaee662f364a30382bbedae6ecdde4010b
sholahul <sholahulfajri30@gmail.com>
2017-10-02 08:04:58 +0700

    Portfolio

---
+++ javasc.js
@@ -0,0 +1,12 @@
+  jQuery(document).ready(function() {
+    jQuery(window).scroll(function() {
+      if ( jQuery(window).width() > 768) {
+        if (jQuery(window).scrollTop() >= 100) {
+            jQuery('#mainnav').css('padding', '5').css('background-color','rgb(40,40,40)');
+             } else {
+            jQuery('#mainnav').css('padding', '20px 0').css('background-color','transparent');
+             }
+      }
+    });
+ });
+
